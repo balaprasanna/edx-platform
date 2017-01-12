TMJUSTNOW/openedx

MA-3091: Added mobile_available flag override admin setting

lms/djangoapps/mobile_api/models.py
@@ -2,8 +2,9 @@
 ConfigurationModel for the mobile_api djangoapp.
 """
 from django.db import models
-from mobile_api import utils
+
 from config_models.models import ConfigurationModel
+from mobile_api import utils
 from mobile_api.mobile_platform import PLATFORM_CLASSES
 
 
@@ -76,3 +77,25 @@ class AppVersionConfig(models.Model):
         """ parses version into major, minor and patch versions before saving """
         self.major_version, self.minor_version, self.patch_version = utils.parsed_version(self.version)
         super(AppVersionConfig, self).save(*args, **kwargs)
+
+
+class MobileAvailableConfig(ConfigurationModel):  # pylint: disable=W5101
+    """
+    Configuration for the mobile_available flag.
+
+    Setting mobile_available_override to true will always grant access in
+    utils.mobile_course_access and will ignore the mobile_available flag
+    when returning the list of enrolled courses in
+    users.views.UserCourseEnrollmentsList
+    """
+    mobile_available_override = models.BooleanField(
+        default=False,
+        help_text="If set, the mobile_available flag is ignored and enables mobiles access for all courses"
+    )
+
+    @classmethod
+    def ignore_mobile_available_flag(cls):
+        """
+        Returns a boolean whether the mobile_available_flag should be ignored
+        """
+        return cls.current().mobile_available_override

lms/djangoapps/mobile_api/testutils.py
@@ -30,6 +30,7 @@ from courseware.access_response import (
 from courseware.tests.factories import UserFactory
 from student import auth
 from student.models import CourseEnrollment
+from mobile_api.models import MobileAvailableConfig
 from mobile_api.tests.test_milestones import MobileAPIMilestonesMixin
 
 
@@ -190,6 +191,26 @@ class MobileCourseAccessTestMixin(MobileAPIMilestonesMixin):
     def test_non_mobile_available(self, role, should_succeed):
         self.init_course_access()
         # set mobile_available to False for the test course
+        self.course.mobile_available = False
+        self.store.update_item(self.course, self.user.id)
+        self._verify_response(should_succeed, MobileAvailabilityError(), role)
+
+    # A tuple of Role Types and Boolean values that indicate whether access should be given to that role.
+    @ddt.data(
+        (auth.CourseBetaTesterRole, True),
+        (auth.CourseStaffRole, True),
+        (auth.CourseInstructorRole, True),
+        (None, True)
+    )
+    @ddt.unpack
+    @patch.dict(settings.FEATURES, {'ENABLE_MKTG_SITE': True})
+    def test_mobile_available_flag_override(self, role, should_succeed):
+        """
+        Verifies when MobileAvailableConfig's mobile_available_override is set
+        to true, course access disregards the mobile_availble flag for all roles.
+        """
+        MobileAvailableConfig(mobile_available_override=True).save()
+        self.init_course_access()
         self.course.mobile_available = False
         self.store.update_item(self.course, self.user.id)
         self._verify_response(should_succeed, MobileAvailabilityError(), role)

balaprasanna/edx-platform

lms/djangoapps/mobile_api/models.py
@@ -2,8 +2,9 @@
 ConfigurationModel for the mobile_api djangoapp.
 """
 from django.db import models
-from mobile_api import utils
+
 from config_models.models import ConfigurationModel
+from mobile_api import utils
 from mobile_api.mobile_platform import PLATFORM_CLASSES
 
 
@@ -18,6 +19,9 @@ class MobileApiConfig(ConfigurationModel):
         blank=True,
         help_text="A comma-separated list of names of profiles to include for videos returned from the mobile API."
     )
+
+    class Meta(object):
+        app_label = "mobile_api"
 
     @classmethod
     def get_video_profiles(cls):
@@ -50,6 +54,7 @@ class AppVersionConfig(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
+        app_label = "mobile_api"
         unique_together = ('platform', 'version',)
         ordering = ['-major_version', '-minor_version', '-patch_version']
 
@@ -76,3 +81,28 @@ class AppVersionConfig(models.Model):
         """ parses version into major, minor and patch versions before saving """
         self.major_version, self.minor_version, self.patch_version = utils.parsed_version(self.version)
         super(AppVersionConfig, self).save(*args, **kwargs)
+
+
+class IgnoreMobileAvailableFlagConfig(ConfigurationModel):  # pylint: disable=W5101
+    """
+    Configuration for the mobile_available flag.
+
+    Setting mobile_available_override to true will always grant access in
+    utils.mobile_course_access and will ignore the mobile_available flag
+    when returning the list of enrolled courses in
+    users.views.UserCourseEnrollmentsList
+    """
+    mobile_available_override = models.BooleanField(
+        default=False,
+        help_text="If set, the mobile_available flag is ignored and enables mobiles access for all courses"
+    )
+
+    class Meta(object):
+        app_label = "mobile_api"
+
+    @classmethod
+    def ignore_mobile_available_flag(cls):
+        """
+        Returns a boolean whether the mobile_available_flag should be ignored
+        """
+        return cls.current().mobile_available_override

lms/djangoapps/mobile_api/testutils.py
@@ -30,6 +30,7 @@ from courseware.access_response import (
 from courseware.tests.factories import UserFactory
 from student import auth
 from student.models import CourseEnrollment
+from mobile_api.models import IgnoreMobileAvailableFlagConfig
 from mobile_api.tests.test_milestones import MobileAPIMilestonesMixin
 
 
@@ -190,6 +191,26 @@ class MobileCourseAccessTestMixin(MobileAPIMilestonesMixin):
     def test_non_mobile_available(self, role, should_succeed):
         self.init_course_access()
         # set mobile_available to False for the test course
+        self.course.mobile_available = False
+        self.store.update_item(self.course, self.user.id)
+        self._verify_response(should_succeed, MobileAvailabilityError(), role)
+
+    # A tuple of Role Types and Boolean values that indicate whether access should be given to that role.
+    @ddt.data(
+        (auth.CourseBetaTesterRole, True),
+        (auth.CourseStaffRole, True),
+        (auth.CourseInstructorRole, True),
+        (None, True)
+    )
+    @ddt.unpack
+    @patch.dict(settings.FEATURES, {'ENABLE_MKTG_SITE': True})
+    def test_mobile_available_flag_override(self, role, should_succeed):
+        """
+        Verifies when IgnoreMobileAvailableFlagConfig's mobile_available_override is set
+        to true, course access disregards the mobile_availble flag for all roles.
+        """
+        IgnoreMobileAvailableFlagConfig(mobile_available_override=True).save()
+        self.init_course_access()
         self.course.mobile_available = False
         self.store.update_item(self.course, self.user.id)
         self._verify_response(should_succeed, MobileAvailabilityError(), role)